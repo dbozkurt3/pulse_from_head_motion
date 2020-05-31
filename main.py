3076cfe7d136df7184a56466e2be2445a2f4a41d
Deniz Bozkurt <oykudenizbozkurt@gmail.com>
2020-05-31 19:58:51 -0400

    Inital commit

---
+++ main.py
@@ -0,0 +1,421 @@
+"""
+Detecting Pulse from Head Motions in Video
+
+This is a pipeline for detecting average pulse rate from video
+based on the paper by Balakrishna et al.
+"""
+
+import sys
+import cv2
+import numpy as np
+import scipy.signal as signal
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+import scipy.stats
+from sklearn.decomposition import PCA
+
+# Haar cascade face classifier
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# Parameters for Shi-Tomasi corner detection
+feature_params = dict( maxCorners = 100,
+                       qualityLevel = 0.05,
+                       minDistance = 5,
+                       blockSize = 3 )
+
+# Parameters for Lucas-Kanade optical flow
+lk_params = dict( winSize  = (15,15),
+                  maxLevel = 2,
+                  criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+
+# Object for principal component analysis
+pca = PCA(n_components=5)
+
+
+def getProcessRegion(frame):
+    """
+    Gets the upper and lower facial regions of interest from the frame.
+    """
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x,y,w,h) in faces:
+        # Calculate subrectangle based on dimensions given in the paper
+        xx = int(x+w*0.25)
+        yy = int(y+h*0.05)
+        ww = int(w/2)
+        hh = int(h*0.9)
+
+        # Calculate the middle rectangle (eye region)
+        ex = xx
+        ey = int(yy+hh*0.20)
+        ew = ww
+        eh = int(hh*0.35)
+
+        # Calculate the upper rectangle (forehead region)
+        ux = xx
+        uy = ey+eh
+        uw = ww
+        uh = int(hh*0.55)
+
+        # Calculate the lower rectangle (mouth region)
+        dx = xx
+        dy = yy
+        dw = ww
+        dh = int(hh*0.20)
+
+    #cv2.rectangle(frame, (xx,yy), (xx+ww, yy+hh), (255,0,0))
+    #cv2.imshow('Box', frame)
+
+    return (yy, ux,uy,uw,uh, ex,ey,ew,eh, dx,dy,dw,dh)
+
+
+def opticalFlow(old_gray, current_frame, p0):
+    """
+    Given an old frame, old feature points, and the current frame,
+    temporally track the feature points in the current frame using
+    the Lucas-Kanade algorithm and return the new points.
+    """
+    frame_gray = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
+
+    # Calculate optical flow
+    p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+
+    # Select good points
+    good_new = p1[st==1]
+    good_old = p0[st==1]
+
+    # Draw the points
+    for i, (new,old) in enumerate(zip(good_new, good_old)):
+        a, b = new.ravel()
+        current_frame = cv2.circle(current_frame, (a,b), 1, (255,0,0), -1) # THIS MODIFIES FRAME
+
+    # Now update the previous frame and previous points
+    old_gray = frame_gray.copy()
+    p0 = good_new.reshape(-1,1,2)
+
+    return (p0, old_gray, good_old, good_new)
+
+
+def cubicSplineInterpolation(src):
+    """
+    Performs cubic spline interpolation on an input time series to increase
+    the sampling rate to that of an ECG device.
+    """
+    outFr = 250  # Desired output resolution = 250 Hz
+    inFr = 30    # The videos were captured at a frame rate of 30 Hz
+    fr = int(outFr/inFr)
+
+    t = np.linspace(0, len(src)-1, num=len(src))
+    f = interp1d(t, src, kind='cubic')
+
+    t_new = np.linspace(0, len(src)-1, num=fr*len(src))
+    resized = f(t_new)
+
+    return resized
+
+
+def stable_features(data):
+    """
+    Filters out erratic points from the data. Erratic points are
+    defined to be points which have a maximum displacement that exceeds
+    the mode of maximum displacements for all points.
+    """
+    motions = np.diff(data, axis=1)
+    max_motions = np.max(motions, axis=1)
+    mode = scipy.stats.mode(max_motions)[0]
+
+    return data[max_motions <= mode, :]
+
+
+def temporalFilter(x):
+    """
+    Applies a 5th order Butterworth filter to a time series
+    in order to filter the signal to a passband of [0.8, 3] Hz.
+    This filter removes low frequency movements like respiration
+    and changes in posture and retains higher frequency movements
+    like pulse and harmonics.
+    """
+    fs = 240
+    nyq = 0.5 * fs
+    f_low = 0.75
+    f_high = 5
+    f_low_dig = f_low/nyq
+    f_high_dig = f_high/nyq
+
+    # Design a Butterworth filter using second-order sections (SOS) for accuracy
+    sos = signal.butter(3, [f_low_dig, f_high_dig], btype='band', output='sos')
+
+    # Apply the filter forward and backwards to remove phase shift
+    y = signal.sosfiltfilt(sos, x)
+
+    return y
+
+
+def PCA_compute(data):
+    """
+    Perform PCA on the time series data and project the points onto the
+    principal axes of variation (eigenvectors of covariance matrix) to get
+    the principal 1-D position signals
+    """
+    temp = data.T  # Arrange the time series data in the format given in the paper
+
+    l2_norms = np.linalg.norm(temp, ord=2, axis=1)  # Get L2 norms of each m_t
+
+    # Discard the points that have L2 norms in the top 25%
+    temp_with_abnormalities_removed = temp[l2_norms < np.percentile(l2_norms, 75)]
+
+    # Fit the PCA model
+    pca.fit(temp_with_abnormalities_removed)
+
+    # Project the tracked point movements on to the principle component vectors
+    projected = pca.transform(temp)
+
+    #mean, eigenvectors = cv2.PCACompute(temp_with_abnormalities_removed, mean=None)
+    #s = np.dot(temp, eigenvectors)
+
+    return projected
+
+
+def signal_selection(s):
+    """
+    Heart rate is calculated by selecting the maximal frequency of the
+    most periodic signal. The periodicity of the signal is defined as the
+    percentage of total spectral power accounted for by the frequency
+    with maximal power and its first harmonic.
+    """
+    fs = 240
+    percentages = np.zeros(5)
+
+    # Only the first 5 source signals are analyzed per the paper
+    for i in range(5):
+        s_i = s[:, i]
+
+        fourier_transform = np.fft.rfft(s_i)
+        power_spectrum = np.square(np.abs(fourier_transform))
+        freqs = np.fft.rfftfreq(len(s_i), d=1./fs)
+
+        total_power = np.sum(power_spectrum)
+        maxInd = np.argmax(power_spectrum)
+        maxFreq = freqs[maxInd]
+        firstHarmonic = 2*maxFreq
+        firstHarmonicInd = np.where(freqs == firstHarmonic)
+
+        percentages[i] = (power_spectrum[maxInd] + power_spectrum[firstHarmonicInd]) / total_power
+    print(percentages)
+    maxPercentInd = np.argmax(percentages)
+    selected_signal = s[:, maxPercentInd]
+
+    selected_fourier_transform = np.fft.rfft(selected_signal)
+    selected_power_spectrum = np.square(np.abs(selected_fourier_transform))
+    selected_freqs = np.fft.rfftfreq(len(selected_signal), d=1./fs)
+
+    plt.figure(5)
+    plt.title('Selected Signal (s{}) Power Spectrum'.format(maxPercentInd + 1))
+    plt.ylabel('Power')
+    plt.xlabel('Frequency')
+    plt.plot(selected_freqs, selected_power_spectrum, 'b')
+
+    selected_maxInd = np.argmax(selected_power_spectrum)
+    fpulse = selected_freqs[selected_maxInd]
+    bpm = 60 / fpulse
+
+    return bpm
+
+    """fs = 240
+
+    # Only the first 5 signals are analyzed per the paper
+    percentages = np.zeros(5)
+    for i in range(5):
+        s_i = s[:, i]
+        #f, Pxx_spec = signal.welch(s_i, fs, 'flattop', 1024, scaling='spectrum')
+        f, Pxx_spec = signal.periodogram(s_i, fs, 'flattop', scaling='spectrum')
+        total_power = np.sum(Pxx_spec)
+
+        [minPower, maxPower, minLoc, maxLoc] = cv2.minMaxLoc(Pxx_spec)
+        freqMaxPower = f[maxLoc[1]]
+        firstHarmonic = 2*freqMaxPower
+
+        firstHarmonicLoc = np.where(f == firstHarmonic)
+        firstHarmonicPower = Pxx_spec[firstHarmonicLoc]
+
+        percentages[i] = (maxPower + firstHarmonicPower)/total_power
+
+    print(percentages)
+    most_periodic_signal = np.argmax(percentages)
+    selected_signal = s[:, most_periodic_signal]
+    #selected_signal = selected_signal - np.mean(selected_signal)  # Remove DC component
+
+    #f, Pxx_spec = signal.welch(selected_signal, fs, 'flattop', 1024, scaling='spectrum')
+    f, Pxx_spec = signal.periodogram(selected_signal, fs, 'flattop', scaling='spectrum')
+
+    [minVal, maxVal, minLoc, maxLoc] = cv2.minMaxLoc(Pxx_spec)
+
+    #print(maxVal)
+    #print(maxLoc)
+    #print(f[maxLoc[1]])
+
+    plt.figure(5)
+    plt.title('Selected Signal (s{}) Power Spectrum'.format(most_periodic_signal+1))
+    plt.semilogy(f, np.sqrt(Pxx_spec))
+    plt.xlabel('Frequency [Hz]')
+    plt.ylabel('Linear spectrum [V RMS]')
+
+    fpulse = f[maxLoc[1]]
+    bpm = 60/fpulse
+
+    return bpm"""
+
+
+##########
+#  MAIN  #
+##########
+if __name__ == '__main__':
+    # Read input video file as first command line argument
+    if len(sys.argv) < 2:
+        print('ERROR: Must provide filename of mp4 file as an argument')
+        sys.exit(1)
+
+    # Create a VideoCapture object and read from input file
+    # If the input is the camera, pass 0 instead of the video file name
+    cap = cv2.VideoCapture(sys.argv[1])
+
+    # Check if camera opened successfully
+    if not cap.isOpened():
+        print("Error opening video stream or file")
+
+    # Track face until video is completed
+    isFirstFrame = True
+    while cap.isOpened():
+        # Capture frame-by-frame
+        ret, frame = cap.read()
+
+        if ret == False:
+            print('End of video')
+            break
+
+        if isFirstFrame:
+            # On the first frame, get the face region
+            yy, ux,uy,uw,uh, ex,ey,ew,eh, dx,dy,dw,dh = getProcessRegion(frame)
+
+            # Create a mask for the regions of interest (forehead and mouth)
+            old_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            face_mask = np.zeros_like(old_gray)
+            face_mask[uy:uy+uh, ux:ux+uw] = 1
+            face_mask[dy:dy+dh, dx:dx+dw] = 1
+
+            # Get points to track in the regions of interest
+            p0 = cv2.goodFeaturesToTrack(old_gray, mask=face_mask, **feature_params)
+
+            # Create a row of data for each feature point
+            num_feature_points = len(p0)
+            data = np.zeros((num_feature_points, 1))
+
+            isFirstFrame = False
+
+        else:
+            # On subsequent frames, track feature points with Lucas-Kanade algorithm
+            p0, old_gray, good_old, good_new = opticalFlow(old_gray, frame, p0)
+
+            # Add each feature point's y-value to a corresponding time series y(t)
+            new_column = np.zeros((num_feature_points, 1))
+            for i in range(len(good_old)):
+                new_column[i, 0] = good_old[i, 1]  # Set a value of column to y-value of the tracked point
+            data = np.append(data, new_column, axis=1)
+
+            # Show the video with tracking points
+            cv2.imshow('Face', frame)
+
+        # Press Q on keyboard to exit
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+    # When video is done, release the video capture object
+    cap.release()
+
+    # Close all the frames
+    cv2.destroyAllWindows()
+
+    # Delete first dummy column from data
+    data = data[:, 1:]
+
+    # Normalize?
+    #r, c = data.shape
+    #for time_series in range(r):
+    #    data[time_series, :] = data[time_series, :] - yy
+
+    # Remove erratic points
+    """print(data.shape)
+    stable_data = stable_features(data)
+    print(stable_data.shape)"""
+    stable_data = data
+
+    # Plot the original time series of a point
+    r, c = stable_data.shape
+    t = np.linspace(0, c-1, num=c)
+    plt.figure(1)
+    plt.title('Original Time Series of a Point')
+    plt.ylabel('Position')
+    plt.xlabel('Time')
+    plt.plot(t, stable_data[0, :], 'b')
+
+    # Signal processing
+    num_row, num_col = stable_data.shape
+    outFr = 250
+    inFr = 30
+    fr = int(outFr/inFr)
+    interpolated_data = np.zeros((num_feature_points, num_col*fr))
+    filtered_data = np.zeros((num_feature_points, num_col*fr))
+
+    for time_series in range(num_row):
+        # Increase the sampling frequency of the data using cubic spline interpolation
+        interpolated_data[time_series, :] = cubicSplineInterpolation(stable_data[time_series, :])
+
+    # Plot interpolated and filtered data
+    r, c = interpolated_data.shape
+    t = np.linspace(0, c - 1, num=c)
+    plt.figure(2)
+    plt.title('Interpolated Time Series')
+    plt.ylabel('Position')
+    plt.xlabel('Time')
+    plt.plot(t, interpolated_data[0, :], 'b')
+
+    for time_series in range(num_row):
+        # Filter out the low frequency movement with Butterworth bandpass filter
+        filtered_data[time_series, :] = temporalFilter(interpolated_data[time_series, :])
+
+    plt.figure(3)
+    plt.title('Interpolated Data Passed Through Filter')
+    plt.plot(t, filtered_data[0, :], 'b')
+
+    """for time_series in range(num_row):
+        # Increase the sampling frequency of the data using cubic spline interpolation
+        interpolated_data[time_series, :] = cubicSplineInterpolation(data[time_series, :])
+
+    print(interpolated_data.shape)
+    stable_data = stable_features(interpolated_data)
+    print(stable_data.shape)
+
+    num_row, num_col = stable_data.shape
+    filtered_data = np.zeros((num_row, num_col))
+    for time_series in range(num_row):
+        # Filter out the low frequency movement with Butterworth bandpass filter
+        filtered_data[time_series, :] = temporalFilter(stable_data[time_series, :])"""
+
+
+    # Compute PCA projection
+    s = PCA_compute(filtered_data)
+
+    # Plot first component signal
+    r, c = s.shape
+    t = np.linspace(0, r-1, num=r)
+    plt.figure(4)
+    plt.title('s1')
+    plt.plot(t, s[:, 0], 'b')
+
+    # Calculate the heart rate in BPM
+    bpm = signal_selection(s)
+    print('Heart rate = {:.2f} BPM'.format(bpm))
+
+    # Display the graphs
+    plt.show()
---
+++ trueBPM.py
@@ -0,0 +1,29 @@
+import sys
+import numpy as np
+import heartpy as hp
+import matplotlib.pyplot as plt
+import json
+
+# Pulse oximeter sample rate = 60 Hz
+sample_rate = 60.0
+  
+# Opening JSON file 
+f = open(sys.argv[1]) 
+ 
+# Returns JSON object as a dictionary 
+data = json.load(f) 
+  
+# Iterating through the json list 
+y = np.empty(len(data['/FullPackage']))
+
+for i in range(len(data['/FullPackage'])):
+    y[i] = data['/FullPackage'][i]['Value']['waveform']
+
+working_data, measures = hp.process(y, sample_rate)
+print(measures['bpm'])
+
+hp.plotter(working_data, measures)
+
+# Closing file 
+f.close() 
+
